05ff072d4222c37ad3e59f6658690f95b232221b
koli9513 <66993009+koli9513@users.noreply.github.com>
2021-08-04 17:21:03 +0200

    feat: add navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React, { Component } from "react";
 import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Navbar from './Components/Navbar'
 
 class App extends Component {
  
   render() {
     return (
     <Router>
-      <div >
-          <p>
+        <div className="App">
+        <div className="container">
+            <Navbar/>
+            <p>
             hello
-          </p>
-      </div>
+            </p>
+        </div>
+        </div>
     </Router>
   );
 }
